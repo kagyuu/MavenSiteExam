4744ff8378de01e724118fd35c7269abb8a2707f
HONDOH Atsushi <kagyuu@hondou.homedns.org>
2017-03-07 01:52:34 +0900

    make jar bundle

--- sub1/src/main/java/com/example/sub1/Sub1App.java
+++ sub1/src/main/java/com/example/sub1/Sub1App.java
@@ -1,5 +1,7 @@
 package com.example.sub1;
 
+import com.example.sub2.Sub2App;
+
 /**
  * Sub1App
  * @author hondou.atsushi
@@ -10,5 +12,9 @@ public class Sub1App {
     }
     public int multi(int x, int y) {
         return x * y;
+    }
+    public int sub(int x, int y) {
+        Sub2App s2 = new Sub2App();
+        return s2.sub(x, y);
     }
 }
